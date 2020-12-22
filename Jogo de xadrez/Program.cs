0bf6865ff4b4da481cc57e744cbc2e089210704b
Maria Luisa Araújo Costa <costamarialuisaa@gmail.com>
2020-12-22 17:19:12 -0300

    Começando a implementar um método que imprime o tabuleiro

--- Jogo de xadrez/Program.cs
+++ Jogo de xadrez/Program.cs
@@ -8,6 +8,9 @@ namespace Jogo_de_xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab); 
+
             Console.ReadLine(); 
         }
     }
