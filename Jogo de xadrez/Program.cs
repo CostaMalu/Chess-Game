31f3dec4cb5b6cd6675bcd4275c73f532fbb0629
Maria Luisa Araújo Costa <costamarialuisaa@gmail.com>
2020-12-22 16:42:48 -0300

    Começando a implementar o tabuleiro

--- Jogo de xadrez/Program.cs
+++ Jogo de xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace Jogo_de_xadrez
 {
@@ -6,7 +7,8 @@ namespace Jogo_de_xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine(); 
         }
     }
 }
